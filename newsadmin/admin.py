987195f282624abafcb116b60c9ec3308704cc46
Chris <cbriant@btinternet.com>
2022-11-19 18:55:08 +0000

    Changed the admin file to display more columns

--- newsadmin/admin.py
+++ newsadmin/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(NewsItem)
+
+class NewsItemAdmin(admin.ModelAdmin):
+    list_display = ('id','source', 'date_added')
+
+admin.site.register(NewsItem,NewsItemAdmin)
 admin.site.register(StoredNewsItem)
